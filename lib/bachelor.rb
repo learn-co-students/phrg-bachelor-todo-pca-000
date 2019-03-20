6eb793cf5f642981a5aa8416b69b6d5ae0df01b1
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-03-20 11:35:08 -0400

    passed test 2

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -13,7 +13,16 @@ end
 
 
 def get_contestant_name(data, occupation)
-  # code here
+  job_search = []
+  data.each do |season, x|
+    data[season].each do |contestant|
+      if contestant.values.include?(occupation)
+        names = contestant["name"]
+        job_search << names
+      end
+    end
+  end
+  job_search.join
 end
 
 def count_contestants_by_hometown(data, hometown)
